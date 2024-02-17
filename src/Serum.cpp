3cb523076cce176814cb8d71ba9ab9829e781a9a
Markus Kalkbrenner <git@kalki.de>
2024-02-17 18:50:53 +0100

    fixed Serum call

--- src/Serum.cpp
+++ src/Serum.cpp
@@ -69,7 +69,7 @@ void Serum::SetStandardPalette(const uint8_t* palette, const int bitDepth)
 bool Serum::ColorizeWithMetadata(uint8_t* frame, int width, int height, uint8_t* palette, uint8_t* rotations,
                                  uint32_t* triggerID, uint32_t* hashcode, int* frameID)
 {
-  return ColorizeWithMetadata(frame, width, height, palette, rotations, triggerID, hashcode, frameID);
+  return Serum_ColorizeWithMetadata(frame, width, height, palette, rotations, triggerID, hashcode, frameID);
 }
 
 }  // namespace DMDUtil
