1fa5cd57324a9bfe1f94459af0b29b62c958387d
Jason Millard <jsm174@gmail.com>
2024-06-24 22:43:41 -0400

    misc: add support for ios simulator platform

--- platforms/ios-simulator/arm64/external.sh
+++ platforms/ios-simulator/arm64/external.sh
@@ -9,13 +9,7 @@ SOCKPP_SHA=e6c4688a576d95f42dd7628cefe68092f6c5cd0f
 LIBPUPDMD_SHA=0eecb74a75c62ff56858a4cd08d82ccce24c4bb8
 LIBFRAMEUTIL_SHA=30048ca23d41ca0a8f7d5ab75d3f646a19a90182
 
-if [[ $(uname) == "Linux" ]]; then
-   NUM_PROCS=$(nproc)
-elif [[ $(uname) == "Darwin" ]]; then
-   NUM_PROCS=$(sysctl -n hw.ncpu)
-else
-   NUM_PROCS=1
-fi
+NUM_PROCS=$(sysctl -n hw.ncpu)
 
 echo "Building libraries..."
 echo "  CARGS_SHA: ${CARGS_SHA}"
@@ -24,7 +18,6 @@ echo "  LIBSERUM_SHA: ${LIBSERUM_SHA}"
 echo "  SOCKPP_SHA: ${SOCKPP_SHA}"
 echo "  LIBPUPDMD_SHA: ${LIBPUPDMD_SHA}"
 echo "  LIBFRAMEUTIL_SHA: ${LIBFRAMEUTIL_SHA}"
-echo "  NUM_PROCS: ${NUM_PROCS}"
 echo ""
 
 if [ -z "${BUILD_TYPE}" ]; then
@@ -47,17 +40,15 @@ curl -sL https://github.com/likle/cargs/archive/${CARGS_SHA}.zip -o cargs.zip
 unzip cargs.zip
 cd cargs-${CARGS_SHA}
 cmake \
-   -DBUILD_SHARED_LIBS=ON \
-   -DCMAKE_SYSTEM_NAME=Android \
-   -DCMAKE_SYSTEM_VERSION=30 \
-   -DCMAKE_ANDROID_ARCH_ABI=arm64-v8a \
-   -DCMAKE_BUILD_WITH_INSTALL_RPATH=TRUE \
-   -DCMAKE_INSTALL_RPATH="\$ORIGIN" \
+   -DCMAKE_SYSTEM_NAME=iOS \
+   -DCMAKE_OSX_SYSROOT=iphonesimulator \
+   -DCMAKE_OSX_ARCHITECTURES=arm64 \
+   -DCMAKE_OSX_DEPLOYMENT_TARGET=17.0 \
    -DCMAKE_BUILD_TYPE=${BUILD_TYPE} \
    -B build
 cmake --build build -- -j${NUM_PROCS}
 cp include/cargs.h ../../third-party/include/
-cp build/*.so ../../third-party/runtime-libs/android/arm64-v8a/
+cp build/*.a ../../third-party/build-libs/ios-simulator/arm64/
 cd ..
 
 #
@@ -67,17 +58,17 @@ cd ..
 curl -sL https://github.com/PPUC/libzedmd/archive/${LIBZEDMD_SHA}.zip -o libzedmd.zip
 unzip libzedmd.zip
 cd libzedmd-$LIBZEDMD_SHA
-platforms/android/arm64-v8a/external.sh
+platforms/ios-simulator/arm64/external.sh
 cmake \
-   -DPLATFORM=android \
-   -DARCH=arm64-v8a \
-   -DBUILD_SHARED=ON \
-   -DBUILD_STATIC=OFF \
+   -DPLATFORM=ios-simulator \
+   -DARCH=arm64 \
+   -DBUILD_SHARED=OFF \
+   -DBUILD_STATIC=ON \
    -DCMAKE_BUILD_TYPE=${BUILD_TYPE} \
    -B build
 cmake --build build -- -j${NUM_PROCS}
 cp src/ZeDMD.h ../../third-party/include/
-cp build/libzedmd.so ../../third-party/runtime-libs/android/arm64-v8a/
+cp build/libzedmd.a ../../third-party/build-libs/ios-simulator/arm64/
 cp -r test ../../
 cd ..
 
@@ -85,20 +76,21 @@ cd ..
 # build libserum and copy to external
 #
 
-curl -sL https://github.com/jsm174/libserum/archive/${LIBSERUM_SHA}.zip -o libserum.zip
+curl -sL https://github.com/zesinger/libserum/archive/${LIBSERUM_SHA}.zip -o libserum.zip
 unzip libserum.zip
 cd libserum-$LIBSERUM_SHA
 cmake \
-   -DPLATFORM=android \
-   -DARCH=arm64-v8a \
-   -DBUILD_SHARED=ON \
-   -DBUILD_STATIC=OFF \
+   -DPLATFORM=ios \
+   -DARCH=arm64 \
+   -DBUILD_SHARED=OFF \
+   -DBUILD_STATIC=ON \
+   -DCMAKE_OSX_SYSROOT=iphonesimulator \
+   -DCMAKE_OSX_DEPLOYMENT_TARGET=17.0 \
    -DCMAKE_BUILD_TYPE=${BUILD_TYPE} \
    -B build
 cmake --build build -- -j${NUM_PROCS}
-cp src/serum.h ../../third-party/include/
 cp src/serum-decode.h ../../third-party/include/
-cp build/libserum.so ../../third-party/runtime-libs/android/arm64-v8a/
+cp build/libserum.a ../../third-party/build-libs/ios-simulator/arm64/
 cd ..
 
 #
@@ -108,38 +100,37 @@ cd ..
 curl -sL https://github.com/fpagliughi/sockpp/archive/${SOCKPP_SHA}.zip -o sockpp.zip
 unzip sockpp.zip
 cd sockpp-$SOCKPP_SHA
-patch -p1 < ../../platforms/android/arm64-v8a/sockpp/001.patch
 cmake \
-   -DSOCKPP_BUILD_SHARED=ON \
-   -DSOCKPP_BUILD_STATIC=OFF \
-   -DCMAKE_SYSTEM_NAME=Android \
-   -DCMAKE_SYSTEM_VERSION=30 \
-   -DCMAKE_ANDROID_ARCH_ABI=arm64-v8a \
-   -DCMAKE_BUILD_WITH_INSTALL_RPATH=TRUE \
-   -DCMAKE_INSTALL_RPATH="\$ORIGIN" \
+   -DSOCKPP_BUILD_SHARED=OFF \
+   -DSOCKPP_BUILD_STATIC=ON \
+   -DCMAKE_SYSTEM_NAME=iOS \
+   -DCMAKE_OSX_SYSROOT=iphonesimulator \
+   -DCMAKE_OSX_ARCHITECTURES=arm64 \
+   -DCMAKE_OSX_DEPLOYMENT_TARGET=17.0 \
    -DCMAKE_BUILD_TYPE=${BUILD_TYPE} \
    -B build
 cmake --build build -- -j${NUM_PROCS}
 cp -r include/sockpp ../../third-party/include/
-cp build/libsockpp.so ../../third-party/runtime-libs/android/arm64-v8a/
+cp build/libsockpp.a ../../third-party/build-libs/ios-simulator/arm64/
 cd ..
 
 #
 # build libpupdmd and copy to external
 #
 
-curl -sL https://github.com/jsm174/libpupdmd/archive/${LIBPUPDMD_SHA}.zip -o libpupdmd.zip
+curl -sL https://github.com/ppuc/libpupdmd/archive/${LIBPUPDMD_SHA}.zip -o libpupdmd.zip
 unzip libpupdmd.zip
 cd libpupdmd-$LIBPUPDMD_SHA
-cmake -DPLATFORM=android \
-   -DARCH=arm64-v8a \
-   -DBUILD_SHARED=ON \
-   -DBUILD_STATIC=OFF \
+cmake \
+   -DPLATFORM=ios-simulator \
+   -DARCH=arm64 \
+   -DBUILD_SHARED=OFF \
+   -DBUILD_STATIC=ON \
    -DCMAKE_BUILD_TYPE=${BUILD_TYPE} \
    -B build
 cmake --build build -- -j${NUM_PROCS}
 cp src/pupdmd.h ../../third-party/include/
-cp build/libpupdmd.so ../../third-party/runtime-libs/android/arm64-v8a/
+cp build/libpupdmd.a ../../third-party/build-libs/ios-simulator/arm64/
 cd ..
 
 #
