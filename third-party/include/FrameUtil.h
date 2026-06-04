2cc4123e333c924cae697eaefe9e1bc0c4b62156
Jan Vos <jjcvos@hotmail.nl>
2026-06-04 17:12:07 +0200

    indentation

--- third-party/include/FrameUtil.h
+++ third-party/include/FrameUtil.h
@@ -275,8 +275,8 @@ inline void Helper::SplitIntoRgbPlanes(const uint16_t* rgb565, int rgb565Size, i
 
 inline float Helper::CalcBrightness(float x)
 {
-    // better spacing between palette entries
-    return x * x * (3.0f - 2.0f * x);
+  // better spacing between palette entries
+  return x * x * (3.0f - 2.0f * x);
 }
 
 inline void Helper::ScaleDownIndexed(uint8_t* pDestFrame, const uint16_t destWidth, const uint8_t destHeight,
